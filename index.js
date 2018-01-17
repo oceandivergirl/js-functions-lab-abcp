11dddb79ffafcc9db685d86de6d2e3f330fd8509
oceandivergirl <oceandivergirl@icloud.com>
2018-01-17 22:30:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
 function happyHolidays(){
   console.out("Happy holidays!")
 }
+function happyHolidaysTo(){
+  console.out(`Happy holidays, ${name}!`)
+}
+function happyHoliday(){
+  console.out(`Happy ${holiday}, ${name}!`)
+}
+function holidayCountdown(){
+  return(`It's ${days} days until ${hiliday}!`)
+}
