732243c99cb13977ceee769c08405f0c177917a4
oceandivergirl <oceandivergirl@icloud.com>
2018-01-17 22:42:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // write your code below!
+var name = "you"
+
+function happyHolidays(){
+  return("Happy holidays!")
+}
+function happyHolidaysTo(){
+  return(`Happy holidays, ${name}!`)
+}
+function happyHolidayTo(){
+  var holiday = "Independence Day"
+  return(`Happy ${holiday}, ${name}!`)
+}
+function holidayCountdown(){
+  var days = 20
+  var holiday = "Mother's Day"
+  return(`It's ${days} days until ${holiday}!`)
+}
